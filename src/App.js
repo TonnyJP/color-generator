0987ca1214fc1e79c66cbfc74d7b3c63d1447382
TonnyJP <tonny.piper@gmx.de>
2023-02-28 10:52:17 +0100

    finished the proj

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React, { useState } from 'react'
+import React from 'react'
 import SingleColor from './SingleColor'
 
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor ] = React.useState('');
+  console.log(color)
+  const [isError, setIsError] = React.useState(false);
+  const [list, setList ] = React.useState(new Values('#f15025').all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      console.log(Values)
+      let colors = new Values(color).all(10);
+      setList([...colors])
+      setIsError(false)
+    } catch (error) {
+      setIsError(true);
+    }
+  }
+  return (
+  <div>
+  <div className='container'>
+    <h3>Color Generator</h3>
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={color} placeholder="#FFFFFF" onChange={(e) => setColor(e.target.value)}className={`${isError?'error': null}`} />
+      <button className='btn' type='submit'>generate</button>
+    </form>
+  </div>
+  <section className="colors">
+      {list.map((color, idx) => {
+        return <SingleColor key={idx} {...color} index={idx} hex={color.hex} />
+      })}
+    </section>
+  </div>)
 }
 
 export default App
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React from 'react'
+//import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({index, rgb, weight, hex}) => {
+  const [ isAlert, setIsAlert ] = React.useState(false);
+  const bcg = rgb.join(',');
+  const hexValue = `#${hex}`;
+  //const hex = rgbToHex(...rgb)
+  const handleClick = () => {
+    setIsAlert(true);
+    navigator.clipboard.writeText(hexValue)
+  }
+
+  React.useEffect(() => {
+    const timer = setTimeout(()=> {
+      setIsAlert(false)
+    }, 3000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  },[isAlert])
+
+  return (
+  <article  className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor: `rgb(${bcg})`}} onClick={handleClick}>
+    <p className="percent-value">{weight}%</p>
+    <p className="color-value">{hexValue}</p>
+    {isAlert && <p className={`alert ${index >10 && 'color-light'}`}>copied to clipboard</p>}
+  </article>)
 }
 
 export default SingleColor
